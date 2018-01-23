898e974f1c3eb40b282211a87587fd5a43dece49
Bjoern Pohl <bjoern@datenwalze.de>
2018-01-23 22:00:04 +0100

    added increase thingy

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ redis = Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'])
 
 @app.route('/')
 def hello_world():
+    redis.incr('hits')
     return '<H1 style="position:fixed; top: 50%; left:50%;transform: translate(-50%, -50%);">Hello from Docker Meetup Bamberg</H1>' + '<br>Container Hostname: ' + socket.gethostname() + '<br>I have been clicked {} times.'.format(redis.get('hits'))
 
 if __name__ == '__main__':
